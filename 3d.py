57a1ce0d5aa98910cdaccb196821cb1aa055fc90
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-09-13 16:58:29 +0100

    Add files via upload

--- 3d.py
+++ 3d.py
@@ -5,40 +5,45 @@ from OpenGL.GLU import *
 
 vertices = (
     (1, -1, -1),
-    (1, -1, 1),
-    (-1, -1, 1),
+    (1, 1, -1),
+    (-1, 1, -1),
     (-1, -1, -1),
-    (0, 1, 0)  # Ponto superior (vértice do triângulo)
+    (1, -1, 4),
+    (1, 1, 4),
+    (-1, -1, 4),
+    (-1, 1, 4)
 )
 
-linhas = (
+arestas = (
     (0, 1),
-    (1, 2),
+    (0, 3),
+    (0, 4),
+    (2, 1),
     (2, 3),
-    (3, 0),
-    (0, 4),  # Linhas do triângulo
-    (1, 4),
-    (2, 4),
-    (3, 4)
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
 )
 
-
-def TrianguloBase():
+def CuboRetangular():
     glBegin(GL_LINES)
-    for linha in linhas:
-        for vertice in linha:
+    for aresta in arestas:
+        for vertice in aresta:
             glVertex3fv(vertices[vertice])
     glEnd()
-
 
 def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
 
-    glTranslatef(0.0, 0.0, -5)
+    
+    glTranslatef(0.0, 0.0, -6.0)  # Posicione a câmera no centro do cubo
 
     while True:
         for event in pygame.event.get():
@@ -46,16 +51,14 @@ def main():
                 pygame.quit()
                 quit()
 
-        glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         # Defina a cor de fundo para amarelo
         glClearColor(1.0, 1.0, 0.0, 1.0)
 
-        TrianguloBase()
+        CuboRetangular()
         pygame.display.flip()
         pygame.time.wait(10)
-
 
 main()
 
